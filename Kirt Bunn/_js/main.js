bbbac6573763c7d340c2472198b6b94ea11ff18c
RoddChames <Rodd@Roddrecuss-MBP.fios-router.home>
2016-12-18 12:11:41 -0500

    Start of Project
    Just imported my canvas to start a new website.

---
+++ Kirt Bunn/_js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#quote').hide();
+	$('#quote').slideDown(3000);
+
+	document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
+
+});
